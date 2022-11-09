c001d252840bac8e5d6b75a9fbd693209e723d91
torrescd <daniel.torrescusa@roche.com>
2022-11-09 14:46:35 +0100

    commit pickle load

--- main.py
+++ main.py
@@ -4,22 +4,15 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 import pickle
+import tarfile
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-t = tarfile.open('model.tar.gz', 'r:gz')
-t.extractall()
-
-model_file_name = 'xgboost-model'
-with open(model_file_name, "rb") as input_file:
-e = pkl.load(input_file)
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    t = tarfile.open('model.tar.gz', 'r:gz')
+    t.extractall()
+
+    model_file_name = 'xgboost-model'
+    with open(model_file_name, "rb") as input_file:
+        e = pickle.load(input_file)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
