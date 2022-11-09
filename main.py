e690092be2aa7e9ddf6c2d5ff1faf53d0e64ac92
torrescd <daniel.torrescusa@roche.com>
2022-11-09 17:07:13 +0100

    predict no crash

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ import numpy as np
 import xgboost
 import pandas
 
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+
 if __name__ == '__main__':
     t = tarfile.open('model.tar.gz', 'r:gz')
     t.extractall()
@@ -18,13 +21,43 @@ if __name__ == '__main__':
         model = xgboost.Booster()
         model.load_model(model_file_name)
 
-        secondrow = pandas.DataFrame()
+        df = pandas.DataFrame([
+            {
+                "readmitted": 'no',
+                "race": 'african_american',
+                "gender": 'female',
+                "age": 25,
+                "time_in_hospital": 2,
+                "num_lab_procedures": 11,
+                "num_procedures": 5,
+                "num_medications": 13,
+                "number_outpatient": 2,
+                "number_emergency": 0,
+                "number_inpatient": 1,
+                "number_diagnoses": 6,
+                "max_glu_serum": 'none',
+                "a1c_result": 'none',
+                "change": 0,
+                "diabetes_med": 1
+            }
+        ])
 
-        test_data = np.array(secondrow, dtype=float)
+        # extract the features and target
+        X_test = df
 
-        data = pandas.DataFrame(np.arange(12).reshape((4, 3)), columns=['a', 'b', 'c'])
-        dtrain = xgboost.DMatrix(data)
+        # one-hot encode the categorical features
+        cat_attribs = ['readmitted', 'race', 'gender', 'max_glu_serum', 'a1c_result']
+        full_pipeline = ColumnTransformer([('cat', OneHotEncoder(handle_unknown='ignore'), cat_attribs)],
+                                          remainder='passthrough')
+
+        encoder = full_pipeline.fit(X_test)
+        X_test_e = encoder.transform(X_test)
 
 
-        model.predict(dtrain)
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+        dtrain = xgboost.DMatrix(X_test_e )
+
+        print(model.get_params(deep=True))
+
+
+        print(model.predict(dtrain))
