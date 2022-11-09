be7b3635d967b791db9840589640444748997fb0
torrescd <daniel.torrescusa@roche.com>
2022-11-09 14:38:18 +0100

    Broken init

---
+++ main.py
@@ -0,0 +1,25 @@
+# This is a sample Python script.
+
+# Press Mayús+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import pickle
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+t = tarfile.open('model.tar.gz', 'r:gz')
+t.extractall()
+
+model_file_name = 'xgboost-model'
+with open(model_file_name, "rb") as input_file:
+e = pkl.load(input_file)
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
